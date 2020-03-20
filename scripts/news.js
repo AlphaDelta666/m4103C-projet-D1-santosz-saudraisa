d9e0c35bdc00edd2589d032c47339c37ce30ac71
Alpha <zikzik93230@gmail.com>
2020-03-20 14:43:45 +0100

    Methode ajout recherche en JS pure terminer

--- scripts/news.js
+++ scripts/news.js
@@ -7,17 +7,46 @@ var recherche_courante_news = [];
 
 
 function ajouter_recherche() {
-	//TODO ...
+	var recherche = document.getElementById('zone_saisie').value;
+  //Retourne la valeur de l'indice de recherches[recherche] ou -1 si il n'y est pas
+  var a = recherches.indexOf(recherche);
+  if(a == -1){
+    //Ajout dans l'array reecherches
+    recherches.push(recherche);
+
+    //Ajout de la div
+    var listeRecherche = document.getElementById('recherches-stockees');
+    //Création de p
+    let newP = document.createElement('p');
+    newP.className = "titre-recherche";
+    //Creation du label
+    let newLabel = document.createElement('label');
+    newLabel.append(recherche);
+    newLabel.setAttribute("onclick", "selectionner_recherche(this)");
+  
+
+    //"Création" de l'image
+    let croix = document.createElement('img')
+    croix.src = "img/croix30.jpg";
+    croix.className = "icone-croix";
+    croix.setAttribute("onclick", "supprimer_recherche(this)");
+
+    //Ajout du label et de l'image a p
+    newP.append(newLabel);
+    newP.append(croix);
+    //Ajout de p dans la liste de recherche
+    listeRecherche.append(newP);
+  }
 }
 
 
 function supprimer_recherche(elt) {
-	//TODO ...
+	console.log("supprimer");
 }
 
 
 function selectionner_recherche(elt) {
-	//TODO ...
+	console.log("selectionner_recherche");
 }
 
 
@@ -44,4 +73,3 @@ function sauver_nouvelle(elt) {
 function supprimer_nouvelle(elt) {
 	//TODO ...
 }
-
