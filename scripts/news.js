95ff46e1291132ed6ac84b009b3a9387b0478fa0
Alpha <zikzik93230@gmail.com>
2020-03-21 16:27:57 +0100

    Ajout methode rechercher_nouvelle et maj_resultats tester et fonctionnelle en JS pure

--- scripts/news.js
+++ scripts/news.js
@@ -6,6 +6,9 @@ var recherche_courante;
 var recherche_courante_news = [];
 //Ensemble des paragraphes
 var listeRecherche = document.getElementById('recherches-stockees');
+//Url des données
+var URL = "https://carl-vincent.fr/search-internships.php?data=";
+
 
 function setP(elt){
 	let newP = document.createElement('p');
@@ -82,11 +85,55 @@ function init() {
 
 
 function rechercher_nouvelles() {
-	//TODO ...
+	//Vidage de la div resultats
+	let resultats = document.getElementById("resultats");
+	resultats.innerHTML = '';
+
+	let wait = document.getElementById("wait");
+	wait.style.display = 'block';
+	let data = document.getElementById("zone_saisie").value
+	console.log(data);
+	ajax_get_request(maj_resultats, URL+data, true);
 }
 
 
 function maj_resultats(res) {
+//On cache le truc de chargement
+	document.getElementById("wait").style.display = 'none';
+	//affichage des résultat
+	//passage de res en format Objets
+	let resultats = JSON.parse(res);
+	let divRes = document.getElementById("resultats");
+	console.log(formatDate(resultats[0].date));
+	for(let i=0; i < resultats.length; i++){
+		let newP = document.createElement('p');
+		newP.className = 'titre_result';
+
+		let newA = document.createElement('a');
+		newA.className = 'titre_news';
+		newA.href = resultats[i].url;
+		newA.target = '_blank';
+		newA.innerHTML = decodeHtmlEntities(resultats[i].titre);
+
+		let spanDate = document.createElement('span');
+		spanDate.className = 'date_news';
+		spanDate.innerHTML = formatDate(resultats[i].date);
+
+		let spanImg = document.createElement('span');
+		spanImg.className = 'action_news';
+		spanImg.onclick = 'sauver_nouvelle(this)';
+
+		let img = document.createElement('img');
+		img.src = 'img/horloge15.jpg';
+
+		spanImg.append(img);
+		newP.append(newA);
+		newP.append(spanDate);
+		newP.append(spanImg);
+
+		divRes.append(newP);
+	}
+
 	//TODO ...
 }
 
@@ -98,4 +145,32 @@ function sauver_nouvelle(elt) {
 
 function supprimer_nouvelle(elt) {
 	//TODO ...
+}
+
+function ajax_get_request(callback, url, async) {
+	// Instanciation d'un objet XHR
+	var xhr = new XMLHttpRequest();
+
+	// Définition de la fonction à exécuter à chaque changement d'état
+	xhr.onreadystatechange = function(){
+		/* readyState permet de connaître l'état de la requête :
+			=> 0: L'objet XHR a été créé, mais pas encore initialisé
+			=> 1: L'objet XHR a été créé, mais pas encore envoyé
+			=> 2: La méthode send vient d'être appelée
+			=> 3: Le serveur traite les informations et a commencé à renvoyer des données
+			=> 4: Le serveur a fini son travail, et toutes les données sont réceptionnées
+		*/
+		if (callback && xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+			// Si une fonction callback est définie + que le serveur a fini son travail
+			// + que le code d'état indique que tout s'est bien passé
+			// => On appelle la fonction callback en passant en paramètre
+			//		les données récupérées sous forme de texte brut
+			callback(xhr.responseText);
+		}
+	};
+
+	// Initialisation de l'objet puis envoi de la requête
+	xhr.open("GET", url, async);
+	xhr.send();
+
 }
